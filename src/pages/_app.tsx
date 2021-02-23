a73355384f2b1fccb6a6aa143d5c15df7b287dc9
Alquipo <alquiponeto@outlook.com.br>
2021-02-23 14:29:34 -0300

    fiz: SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app'
-import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
@@ -8,20 +7,6 @@ import theme from 'styles/theme'
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <title>MoveIt</title>
-        <link rel="shortcut icon" href="/img/nextjs-logo.svg" />
-        <link rel="apple-touch-icon" href="/img/nextjs-logo.svg" />
-        <link rel="manifest" href="/manifest.json" />
-
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@700&display=swap"
-          rel="stylesheet"
-        />
-
-        <meta name="description" content="" />
-      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,20 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <title>MoveIt</title>
+          <link rel="shortcut icon" href="/img/nextjs-logo.svg" />
+          <link rel="apple-touch-icon" href="/img/nextjs-logo.svg" />
+          <link rel="manifest" href="/manifest.json" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@700&display=swap"
+            rel="stylesheet"
+          />
+
+          <meta name="description" content="" />
+        </Head>
         <body>
           <Main />
           <NextScript />
