172862d56187d4bcad2532e47819caecd663ca37
Alquipo <alquiponeto@outlook.com.br>
2021-02-24 02:54:43 -0300

    fix: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ module.exports = {
     '../src/components/**/stories.mdx',
     '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['themeprovider-storybook/register', '@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,36 @@
 import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
-
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from 'styles/global'
-import theme from 'styles/theme'
+import { withThemesProvider } from 'themeprovider-storybook'
+
+import { combineTheme, dark, light } from 'styles/themes'
 
 addDecorator(withNextRouter())
 
+const themes = [
+  {
+    ...dark,
+    backgroundColor: '#121214'
+  },
+  {
+    ...light,
+    backgroundColor: '#F2F3F5'
+  }
+]
+
+export const parameters = {
+  backgrounds: { disable: true }
+}
+
 export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Story />
-    </ThemeProvider>
-  )
+  withThemesProvider(themes, {
+    CustomThemeProvider: ThemeProvider
+  }),
+  (Story) => {
+    return (
+      <ThemeProvider theme={combineTheme(dark)}>
+        <Story />
+      </ThemeProvider>
+    )
+  }
 ]
