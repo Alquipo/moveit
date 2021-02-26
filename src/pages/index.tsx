9744308d069fbd23e08a2433231fac664bf253a7
Alquipo <alquiponeto@outlook.com.br>
2021-02-26 13:18:26 -0300

    feat: notification and cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,3 +3,9 @@ import HomeTemplate from 'templates/Home'
 export default function Home() {
   return <HomeTemplate />
 }
+
+export const getServerSideProps = async () => {
+  return {
+    props: {}
+  }
+}
