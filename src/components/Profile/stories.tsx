aea6a08ff067ec4beaaccd657eedd2c757b169b0
Alquipo <alquiponeto@outlook.com.br>
2021-02-27 02:32:20 -0300

    fix: stories

--- src/components/Profile/stories.tsx
+++ src/components/Profile/stories.tsx
@@ -1,9 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Profile from '.'
+import Profile, { ProfileProps } from '.'
 
 export default {
   title: 'Profile',
   component: Profile
 } as Meta
 
-export const Default: Story = () => <Profile />
+export const Default: Story<ProfileProps> = (props) => <Profile {...props} />
