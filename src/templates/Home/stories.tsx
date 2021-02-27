bed4c9582b8f687d60a83a8472cdaef26943d855
Alquipo <alquiponeto@outlook.com.br>
2021-02-26 21:25:54 -0300

    fix

--- src/templates/Home/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Home from '.'
-
-export default {
-  title: 'Home',
-  component: Home
-} as Meta
-
-export const Default: Story = () => <Home />
