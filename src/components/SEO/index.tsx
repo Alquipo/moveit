1b7e262654bb3eb64037b7886948889f98443750
Alquipo <alquiponeto@outlook.com.br>
2021-02-28 14:22:13 -0300

    fix: SEO

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -36,6 +36,15 @@ export default function SEO({
         content="width=device-width, initial-scale=1, user-scalable=no"
       />
 
+      <meta name="full-screen" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="MoveIt" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+
       <meta name="MobileOptimized" content="320" />
       <meta name="HandheldFriendly" content="True" />
       <meta name="theme-color" content="#121214" />
