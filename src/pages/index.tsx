1d7189ef17a5a80247e89a6ca6052b3195e222c8
Alquipo <alquiponeto@outlook.com.br>
2021-02-28 19:23:42 -0300

    fix: indexpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
 import { useAuth } from 'contexts/AuthContext'
-import { GetStaticProps } from 'next'
 import React from 'react'
-import DashboardTemplate, { DashboardTemplateProps } from 'templates/Dashboard'
+import DashboardTemplate from 'templates/Dashboard'
 import HomeTemplate from 'templates/Home'
 
-export default function Home(props: DashboardTemplateProps) {
+export default function Home() {
   const { isLogged, userData } = useAuth()
 
   return (
     <>
       {userData === null && <HomeTemplate />}
-      {isLogged && <DashboardTemplate {...props} />}
+      {isLogged && <DashboardTemplate />}
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    props: {}
-  }
-}
+// export const getStaticProps: GetStaticProps = async () => {
+//   return {
+//     props: {}
+//   }
+// }
